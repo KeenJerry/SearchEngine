b0ed4612423665957c82ba164bffac50c52d2d2f
ZhouKan <3286307856@qq.com>
2018-06-26 19:06:58 +0800

    modify Trie Tree

--- SearchEngine/tools.py
+++ SearchEngine/tools.py
@@ -1,17 +1,20 @@
 
 class TrieTree:
 
-    root = {}
-    end = '/'
-
     def __init__(self):
-        pass
+        self.root = TrieNode()
 
-    def add_term(self, term):
+    def add_term(self, term, file_name):
         node = self.root
         for char in term:
-            node = node.setdefault(char, {})
-        node[self.end] = None
+            for child in node.children:
+                if char == child.char:
+                    node = child
+                    break
+            new_node = TrieNode(char, file_name)
+            node.children.append(new_node)
+            node = new_node
+        node.is_term = True
 
     def find_term(self, term):
         # TODO implement
@@ -20,7 +23,14 @@ class TrieTree:
 
 class TrieNode:
 
-    ptr_frequency = None
+    is_term = False
+    char = None
+    file_name = None
+    children = []
 
-    ptr_position = None
 
+    # TODO need more details
+
+    def __init__(self, char, file_name):
+        self.char = char
+        self.file_name = file_name
